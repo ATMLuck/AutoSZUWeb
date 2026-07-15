f6e7fc2006668104b234a9cbf8700e93c193a911
ATMLuck <A.T.M.Luck@outlook.com>
2026-07-15 16:28:18 +0800

    framework_update

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 windres ico.rc -o ico.o
-g++ -std=c++17 -g -static -DCURL_STATICLIB -I ./include ^
-    -L ./lib AutoSZUWeb.cpp ico.o -lcurl -lssl -lcrypto ^
+g++ -std=c++17 -static -DCURL_STATICLIB -I ./include ^
+    ./stc/AutoSZUWeb.cpp ico.o ^
+    -lcurl -lssl -lcrypto ^
     -lnghttp2 -lnghttp3 -lngtcp2 -lngtcp2_crypto_libressl ^
     -lssh2 -lz ^
     -lzstd -lbrotlidec -lbrotlicommon -lpsl ^
